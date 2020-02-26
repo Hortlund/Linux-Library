a354f4dc6fdc891908aa3058d72bd73f14d0d1de
samkal-7 <samkal-7@student.ltu.se>
2020-02-26 15:34:37 +0100

    Fixed nullpointer

--- libresistance.c
+++ libresistance.c
@@ -3,28 +3,27 @@
 float calc_resistance(int count, char conn, float *array){
 	float sumOhm = 0, tempOhm = 0, divideOhm = 0;
 
-	if(conn == 'S'){
-		for(int i = 0; i < count; i++){
-			sumOhm += array[i];
+	if(array != NULL){
+		if(conn != 'S' && conn != 'P'){
+        	        return -1;
+        	}
+		if(conn == 'S'){
+			for(int i = 0; i < count; i++){
+				sumOhm += array[i];
+			}
+			return sumOhm;
 		}
-		return sumOhm;
-	}
-	else if(conn == 'P'){
-		for(int i = 0; i < count; i++){
-			tempOhm += (1 / array[i]);
+		else if(conn == 'P'){
+			for(int i = 0; i < count; i++){
+				if(array[i]!=0)
+					tempOhm += (1 / array[i]);
+				else
+					return 0
+			}
+			divideOhm = 1 / tempOhm;
+			sumOhm = divideOhm;
+			return sumOhm;
 		}
-		divideOhm = 1 / tempOhm;
-		sumOhm = divideOhm;
-		return sumOhm;
-	}
-	if(!isdigit(count)){ 
-                return -1;
-        }
-        else if(!isalpha(conn)){
-                return -1;
-        }
-        else if(conn != 'S' && conn != 'P'){
-                return -1;
-        }
-	return 0;
+	}	
+		return 0;
 }
